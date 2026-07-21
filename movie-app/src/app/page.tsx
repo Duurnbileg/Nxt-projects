9a431018d48c5a8d3bdefcce00c6923996a6dc43
Duurnbileg <duurnbileg@gmail.com>
2026-07-21 10:19:18 +0800

    add bug

--- movie-app/src/app/page.tsx
+++ movie-app/src/app/page.tsx
@@ -3,6 +3,8 @@ import Hero from "./features/hero";
 import MovieCard from "./features/movieCard";
 
 export default function Home() {
+  const APIkey = "a2e46a074cd6c332859a4f1111baf7d5"
+
   return (
     <main className="flex items-center justify-center">
       <div className="w-[1520px] flex flex-col items-center justify-center gap-2">
